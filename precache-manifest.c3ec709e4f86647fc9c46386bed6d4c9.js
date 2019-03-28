4f2dcc0fe4f6928c9d840fa00fd4c9f70a31eefa
Khari <khari.lane@khalikenterprises.com>
2019-03-28 09:41:27 -0500

    Updates

---
+++ precache-manifest.c3ec709e4f86647fc9c46386bed6d4c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "81ba3bc20c740d0ff0293a4f06a734b1",
+    "url": "/QuoteMachine/static/media/madRick.81ba3bc2.png"
+  },
+  {
+    "revision": "476111808bc1d491d5b107d8b6f175b5",
+    "url": "/QuoteMachine/static/media/happyRick.47611180.png"
+  },
+  {
+    "revision": "ab1f7b065459d22f075b",
+    "url": "/QuoteMachine/static/js/runtime~main.573c12d3.js"
+  },
+  {
+    "revision": "7118ea308d53264ba2a7",
+    "url": "/QuoteMachine/static/js/main.ccec2f4d.chunk.js"
+  },
+  {
+    "revision": "0473bd11d687d5436580",
+    "url": "/QuoteMachine/static/js/2.4476e10e.chunk.js"
+  },
+  {
+    "revision": "7118ea308d53264ba2a7",
+    "url": "/QuoteMachine/static/css/main.db394f25.chunk.css"
+  },
+  {
+    "revision": "cd222d69f573fccd4e20364a8e580be5",
+    "url": "/QuoteMachine/index.html"
+  }
+];
